dc48fe4f2c9eb98fed3128a71c41e1e31b0bf011
Danli Denny Deng <danlideng@yahoo.com>
2022-02-20 10:48:05 -0800

    finished lesson 27

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import CakeContainer14 from './components/CakeContainer14';
 import HooksCakeContainer20 from './components/HooksCakeContainer20';
 import IceCreamContainer22 from './components/IceCreamContainer22';
 import NewCakeContainer25 from './components/NewCakeContainer25';
+import ItemContainer26 from './components/ItemContainer26';
+import ItemContainer27 from './components/ItemContainer27';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+
+      <ItemContainer27 cake />
+        <ItemContainer27 />
+
+        <ItemContainer26 cake />
+        <ItemContainer26 />
         <HooksCakeContainer20 />
 
         <CakeContainer14 /> 
--- src/components/ItemContainer26.js
+++ src/components/ItemContainer26.js
@@ -1,9 +1,20 @@
 import React from 'react' 
+import { connect } from 'react-redux'
 
-function ItemContainer() {
+function ItemContainer26(props) {
   return (
     <div>
-      
+      <h2>Item - {props.item}</h2>
     </div>
   )
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const itemState = ownProps.cake ? state.cake.numOfCakes : state.iceCream.numOfIceCreams
+
+  return {
+    item: itemState
+  }
+}
+
+export default connect(mapStateToProps)(ItemContainer26)
